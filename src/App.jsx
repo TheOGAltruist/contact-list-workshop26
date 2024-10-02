1e997bfa0ce963c0cbf6b211d109d02cd64c4c1d
TheOGAltruist <staraknath@yahoo.co.in>
2024-10-02 01:52:23 +0000

    Added first few components to render initial state with dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import ContactList from './components/ContactList'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <ContactList />
     </>
   )
 }
